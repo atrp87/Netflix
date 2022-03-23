36b3eceeb54c628f9cd7be2b056c14e82bd693a8
Andrew Peattie <drewpeattie@hotmail.com>
2022-03-23 10:52:26 +0000

    header skeleton prep

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,13 +4,16 @@ import { Link } from 'react-router-dom'
 export default function Header({ children }) {
   return (
     <header className='header'>
+      <div className="gradient_overlay"></div>
       <nav className='navbar'>
-        <img
-          src={Logo}
-          alt="Netflix logo"
-          className='logo'
-        />
-        <Link to='signin'>Sign In</Link>
+        <div className="nav_items">
+          <img
+            src={Logo}
+            alt="Netflix logo"
+            className='logo'
+          />
+          <Link to='signin'>Sign In</Link>
+        </div>
       </nav>
       {children}
     </header>
