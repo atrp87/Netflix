87c799cbf5790f44d65189abb363a069129adbfa
Andrew Peattie <drewpeattie@hotmail.com>
2022-03-24 12:52:50 +0000

    header final

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,20 +1,25 @@
 import Header from '../../components/header/Header'
 import Hero from '../../components/hero/Hero'
 import CtaForm from '../../components/cta-form/CtaForm'
+import Jumbotron from '../../components/jumbotron/Jumbotron'
 
 
 export default function Home() {
   return (
-    <section className='home'>
+    <div className='home'>
       <Header>
         <Hero>
           <CtaForm />
         </Hero>
       </Header>
 
-      {/* <Jumbotron />
-      <Accordion />
+      <Jumbotron />
+
+
+
+
+      {/* <Accordion />
       <Footer /> */}
-    </section>
+    </div>
   )
 }
